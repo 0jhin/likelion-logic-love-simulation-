674c9572a8c185b05cb2e95bad957d580f7ab524
0jhin <casper118412@gmail.com>
2025-07-15 16:08:21 +0900

    알고리즘 완성

---
+++ app/src/main/java/com/example/myapplication2/SecondMain.kt
@@ -0,0 +1,90 @@
+package com.example.myapplication2
+
+fun main() {
+
+    // 0 ~ 100 사이의 정답이 있음
+    // 20번 반복
+    // 사용자 입력 받음
+    // 정답보다 작으면 "업" 출력
+    // 정답보다 높으면 "다운" 출력
+    // 정답이면 종료
+
+
+    var userCount = 20 // 남은 횟수
+    val userUp = "업"
+    val userDown = "다운"
+    val description : String = "0부터 100까지의 숫자를 정합니다. 기회는 20번이며 정답에 입력한 숫자를 업 다운으로 힌트를 드립니다.(숫자만 넣으세요)"
+    val gameModeDescription : String = "게임 모드를 설정합니다. [설정], [게임]"
+
+
+    println(gameModeDescription)
+    var gameMode : String? = readLine()
+    if (gameMode == "설정"){
+        println("설정모드로 진입합니다.")
+    } else {
+        if (gameMode == "게임"){
+            println(description) // 게임 설명
+            print("정답을 숫자로 입력하세요 : ")
+
+            var correct = readLine() // 정답 입력
+            var adminInputNum = correct?.toInt() // 정수 변환
+
+            while (0 < userCount) {
+                println("정답을 맞춰보세요")
+                var userInput = readLine() // 받은 입력
+                var userInputNum = userInput?.toInt() // 정수 변환
+
+                if (userInputNum != null && adminInputNum != null) {
+                    if (userInputNum < adminInputNum) {
+                        println(userUp)
+                    } else if (userInputNum > adminInputNum) {
+                        println(userDown)
+                    } else {
+                        println("정답")
+                        break
+                    }
+                }
+                userCount -= 1
+                println("남은 횟수 : $userCount")
+            }
+        }
+    }
+
+
+}
+
+
+
+
+//fun main() {
+//
+//    var someValue1: Int = 123
+//    var someValue2: String = "123"
+//    var someValue3: Boolean = true
+//    var someValue4: Any = "dsadw"
+//    var someValue5: String? = null
+//    var someValue6: Any? = null
+//
+//    var someObject : Any = Any()
+//
+//    var otherObject = someObject
+//
+//    var someArray =arrayListOf(1,2,3)
+//
+//    println("Hello World")
+//}
+
+
+//fun main() {
+//    while (true){
+//        readLine()
+//        println("[TEST 6]")
+//        println("작업중")
+//        println("[TEST 8]")
+////        break
+//        return
+//        println("[TEST 10]")
+//    }
+//
+//    println("[TEST 13]")
+//}
---
+++ app/src/main/java/com/example/myapplication2/test.kt
@@ -0,0 +1,13 @@
+//package com.example.myapplication2
+//
+//fun main() {
+//    print("숫자를 입력하세요: ")
+//    val input = readLine()           // 문자열로 입력 받기
+//    val number = input?.toInt()      // null 체크 후 정수로 변환
+//
+//    if (number != null) {
+//        println("입력한 숫자는 $number 입니다.")
+//    } else {
+//        println("유효한 숫자가 아닙니다.")
+//    }
+//}
